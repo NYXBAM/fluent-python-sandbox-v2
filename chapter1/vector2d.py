7c6dd2780d1f3eed8de12a5933f8054e6b1abbca
NYXBAM <delirium.dev@proton.me>
2025-11-26 00:00:28 -0800

    finish chapter 1

--- chapter1/vector2d.py
+++ chapter1/vector2d.py
@@ -1,12 +1,13 @@
-import math 
+import math
+from typing import Any 
 
 class Vector:
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
-    
+        
     def __repr__(self):
-        return f'Vector ({self.x!r}, {self.y!r})'
+        return f"Vector ({self.x!r}, {self.y!r})"
     
     def __abs__(self):
         return math.hypot(self.x, self.y)
@@ -15,7 +16,7 @@ class Vector:
         return bool(abs(self))
     
     def __add__(self, other):
-        x = self.x + other.x 
+        x = self.x + other.x
         y = self.y + other.y
         return Vector(x, y)
     
