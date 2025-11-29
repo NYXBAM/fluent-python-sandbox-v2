d85c3484891ff600efa33557d0d0b804dc7e18b8
NYXBAM <delirium.dev@proton.me>
2025-11-29 00:39:28 -0800

    tuple unpacking

---
+++ chapter2/examples.py
@@ -0,0 +1,46 @@
+
+# tuple unpacking
+
+a, b, *rest = range(5)
+print(a, b, rest) # 0 1 [2, 3, 4]
+a, b, *rest = range(3)
+print(a, b, rest) # 0 1 [2]
+a, b, *rest = range(2)
+print(a, b, rest) # 0 1 []
+
+a, *body, c, d = range(5)
+print(a, body, c, d) # 0 [1, 2] 3 4
+*head, b, c, d = range(5)
+print(head, b, c, d) # [0, 1] 2 3 4
+
+def fun(a, b, c, d, *rest):
+    return a, b, c, d, rest
+
+print(fun(*[1,2], 3, *range(4,7)))  # (1, 2, 3, 4, (5, 6))
+
+
+metro_areas = [
+    ('Tokyo', 'JP', 36.933, (35.689722, 139.691667)),
+    ('Delhi NCR', 'IN', 21.935, (28.613889, 77.208889)),
+    ('Mexico City', 'MX', 20.142, (19.433333, -99.133333)),
+    ('New York-Newark', 'US', 20.104, (40.808611, -74.020386)),
+    ('Sao Paulo', 'BR', 19.649, (-23.547778, -46.635833)),
+]
+
+def main():
+    print(f'{"":15} | {"latitude":>9} | {"longitude":9}')
+    for name, _, _, (lat, lon) in metro_areas:
+        if lon <= 0:
+            print(f'{name:15} | {lat:9.4f} | {lon:9.4f}')
+            
+            '''
+                            |  latitude | longitude
+            Mexico City     |   19.4333 |  -99.1333
+            New York-Newark |   40.8086 |  -74.0204
+            Sao Paulo       |  -23.5478 |  -46.6358
+                        
+            '''
+            
+
+if __name__ == '__main__':
+    main()
