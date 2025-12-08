3c4d612bab0519350031e859de348757ed1f3622
NYXBAM <delirium.dev@proton.me>
2025-12-08 01:04:32 -0800

    chapter3

---
+++ chapter3/creator.py
@@ -0,0 +1,37 @@
+def get_creators(record: dict) -> list:
+    match record:
+        case {'type': 'book', 'api': 2, 'authors': [*names]}:
+            return names
+        case {'type': 'book', 'api': 1, 'author': name}:
+            return [name]
+        case {'type': 'book'}:
+            raise ValueError(f"Invalid 'book' record: {record!r}")
+        case {'type': 'movie', 'director': name}:
+            return [name]
+        case _:
+            raise ValueError(f'Invalid record: {record}')
+        
+        
+
+b1 = dict(api=1, author='Douglas Hofstadter', 
+          type='book', title='Godel, Escher, Bach')
+
+print(get_creators(b1)) # ['Douglas Hofstadter']
+
+
+from collections import OrderedDict
+
+b2 = OrderedDict(api=2, type='book',
+                 title='Python in a Nutshell',
+                 authors='Martelli Ravenscroft Holden'.split())
+
+print(get_creators(b2)) # ['Martelli', 'Ravenscroft', 'Holden']
+
+
+food = dict(category='ice cream', flavor='vanilla',
+            cost=199)
+
+match food:
+    case {'category': 'ice cream', **details}:
+        print(f'Ice cream details {details}') # Ice cream details {'flavor': 'vanilla', 'cost': 199}
+        
---
+++ chapter3/dictcomp.py
@@ -0,0 +1,35 @@
+dial_codes = [
+    (880, 'Bangladesh'),
+    (55, 'Brazil'),
+    (86, 'China'),
+    (62, "Indonesia"),
+    (81, 'Japan'),
+    (234, 'Nigeria'),
+    (92, 'Pakistan'),
+    (7, 'russia'),
+    (1, 'United States'),
+    ]
+
+country_dial = {country: code for code, country in dial_codes}
+print(country_dial) # {'Bangladesh': 880, 'Brazil': 55, 'China': 86, 'Indonesia': 62, 'Japan': 81, 'Nigeria': 234, 'Pakistan': 92, 'russia': 7, 'United States': 1}
+c = {code: country.upper() for country, code in sorted(country_dial.items()) if code < 70}
+print(c)
+
+def dump(**kwargs):
+    return kwargs
+
+print(dump(**{'x': 1}, y=2, **{'z':3})) # {'x': 1, 'y': 2, 'z': 3}
+print({'a': 0, **{'x':1}, 'y':2, **{'z':3, 'x':4}}) # {'a': 0, 'x': 4, 'y': 2, 'z': 3}
+
+d1 = {'a': 1, 'b': 3}
+d2 = {'a': 2, 'b': 4, 'c': 6}
+ 
+print(d1|d2) # {'a': 2, 'b': 4, 'c': 6}
+
+d1|=d2
+print(d1) # {'a': 2, 'b': 4, 'c': 6}
+
+
+
+
+
