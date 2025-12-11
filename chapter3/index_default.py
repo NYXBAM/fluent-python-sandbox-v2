cbb738edb2835084513908484d63ef6aa722b487
NYXBAM <delirium.dev@proton.me>
2025-12-11 02:47:19 -0800

    dict, default dict

---
+++ chapter3/index_default.py
@@ -0,0 +1,30 @@
+import collections
+import re 
+import sys 
+
+WORD_RE = re.compile(r'\w+')
+
+index = collections.defaultdict(list)
+
+with open(sys.argv[1], encoding='utf-8') as fp:
+    for line_no, line in enumerate(fp, 1):
+        for match in WORD_RE.finditer(line):
+            word = match.group()
+            column_no = match.start() + 1
+            location = (line_no, column_no)
+            index[word].append(location)
+            
+            
+for word in sorted(index, key=str.upper):
+    print(word, index[word])
+    
+'''
+AMAZING [(3, 14)]
+book [(3, 6)]
+great [(2, 10)]
+Hello [(1, 1)]
+is [(2, 7), (3, 11)]
+This [(3, 1)]
+world [(1, 7)]
+World [(2, 1)]
+'''
