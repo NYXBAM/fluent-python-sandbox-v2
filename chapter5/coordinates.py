a9f4b9d87b23e33785a00379dd91a8890d18664b
NYXBAM <delirium.dev@proton.me>
2026-01-05 00:58:00 -0800

    trying to adding namedtuple methods; starting typing annotation (type hints)

--- chapter5/coordinates.py
+++ chapter5/coordinates.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+from locale import currency
 
 
 City = namedtuple("City", "name country population coordinates", defaults=('Test', 'test', 'test', 'test'))
@@ -29,3 +30,48 @@ print(test2) # City(name='Test2', country='CN', population='test', coordinates='
 import json
 
 print(json.dumps(delhi._asdict())) # {"name": "Delhi NCR", "country": "IN", "population": 21.935, "coordinates": [28.6139, 77.209]}
+
+
+Coordinate = namedtuple("Coordinate", 'lat lon reference', defaults=["WGS84"])
+
+print(Coordinate(0, 0)) # Coordinate(lat=0, lon=0, reference='WGS84')
+
+
+# Namedtuple with methods 
+        
+from collections import namedtuple
+
+
+Item = namedtuple('Item', ['name', 'price'])
+
+iphone = Item('iPhone 15', 1000)
+milk = Item('Moloko', 2)
+
+
+def get_pretty_price(self):
+    return f"${self.price}.00 USD"
+
+
+def get_discount_price(self):
+    return self.price * 0.9
+
+
+Item.pretty_price = get_pretty_price
+Item.discount = get_discount_price
+
+print(f"{iphone.name} : {iphone.discount()}") 
+
+print(Item.__dict__)
+
+# Or better readable way 
+
+class Item(namedtuple('Item', ['name', 'price'])):
+    currency = '$'
+    
+    def pretty_price(self):
+        return f"{self.currency}{self.price}.00"
+
+    def discount(self, percent=10):
+        return self.price * (1 - percent/100)
+    
+    
