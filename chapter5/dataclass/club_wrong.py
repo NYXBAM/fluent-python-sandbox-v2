5d5ad7ac86cfad2b9201fd8d1e771b5fbed74e97
NYXBAM <delirium.dev@proton.me>
2026-01-07 01:17:40 -0800

    Default Fields in dataclasses:
    default_factory and etc,

---
+++ chapter5/dataclass/club_wrong.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass, field
+from pickle import FALSE
+
+def test_create_list() -> list:
+    return [1,2,3,4,5,6]
+
+
+@dataclass
+class ClubMember:
+    name: str
+    guests: list = field(default_factory=test_create_list)
+
+
+s = ClubMember(name='1')
+print(s)
+
+
+@dataclass
+class ClubMember1:
+    name: str
+    guest: list[str] = field(default_factory=list)
+    athlete: bool = field(default=False, repr=False)
+    
+cm = ClubMember1(name='john',
+                 guest=['Doe','Hue', 'Jan'])
+
+print(cm)
+print(cm.athlete)
