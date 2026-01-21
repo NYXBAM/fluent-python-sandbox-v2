e08e15c93d48818d50f9694cd247bab3573bcd0d
NYXBAM <delirium.dev@proton.me>
2026-01-21 00:09:31 -0800

    operator itemgetter

---
+++ chapter7/operator.py
@@ -0,0 +1,41 @@
+from functools import reduce
+
+def factorial(n):
+    return reduce(lambda a, b: a*b, range(1, n+1))
+
+# Better way, use reduce and operator.mul
+from operator import mul, itemgetter
+
+def factorial2(n):
+    return reduce(mul, range(1, n+1))
+
+
+
+# itemgetter
+
+metro_data = [
+    ("Tokyo", "JP", 36.933, (35.689722, 139.691667)),
+    ("Delhi NCR", "IN", 21.935, (28.613889, 77.208889)),
+    ("Mexico City", "MX", 20.142, (19.433333, -99.133333)),
+    ("New York-Newark", "US", 20.104, (40.808611, -74.020386)),
+    ("São Paulo", "BR", 19.649, (-23.547778, -46.635833)),
+]
+
+
+for city in sorted(metro_data, key=itemgetter(1)):
+    print(city)
+# ('São Paulo', 'BR', 19.649, (-23.547778, -46.635833))
+# ('Delhi NCR', 'IN', 21.935, (28.613889, 77.208889))
+# ('Tokyo', 'JP', 36.933, (35.689722, 139.691667))
+# ('Mexico City', 'MX', 20.142, (19.433333, -99.133333))
+# ('New York-Newark', 'US', 20.104, (40.808611, -74.020386))
+ 
+cc_name = itemgetter(1,0) # We can put here any indexes
+
+for city in metro_data:
+    print(cc_name(city))
+# ('JP', 'Tokyo')
+# ('IN', 'Delhi NCR')
+# ('MX', 'Mexico City')
+# ('US', 'New York-Newark')
+# ('BR', 'São Paulo')
