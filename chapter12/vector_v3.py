42059f5160e0de4b99bb6e2549071c1e7c13d360
NYXBAM <delirium.dev@proton.me>
2026-03-01 00:09:22 -0800

    vector: __setattr__ examples

--- chapter12/vector_v3.py
+++ chapter12/vector_v3.py
@@ -46,6 +46,7 @@ class Vector:
             
     def __getattr__(self, name):
         cls = type(self)
+        error = None
         try:
             pos = cls.__match_args__.index(name)
         except ValueError:
@@ -55,14 +56,46 @@ class Vector:
         msg = f'{cls.__name__!r} object has no attribute {name!r}'
         raise AttributeError(msg)
     
+    def __setattr__(self, name, value):
+        cls = type(self)
+        if len(name) == 1:
+            if name in cls.__match_args__:
+                error = f'readonly attribute {name!r}'
+            elif name.islower():
+                error = 'can`t set attributes \'a\' to \'z\' in {cls.__name__!r}'
+            else: error = ''   
+            if error:
+                msg = error.format(cls_name=cls.__name__, attr_name=name)
+                raise AttributeError(msg)
+            
+        super().__setattr__(name, value)
+        
+        
     
-v = Vector(range(10))
-print(v.y, v.z) # 1.0 2.0
+# v = Vector(range(10))
+# print(v.y, v.z) # 1.0 2.0
 
-print(v) # (0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0)
-v.x = 10 
-print(v.x) # 10
-print(v[0]) # 0.0
-print(v.__dict__) # {'_components': array('d', [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]), 'x': 10}
-print(v) # (0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0)
+# print(v) # (0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0)
+# # v.x = 10 
+# print(v.x) # 10
+# print(v[0]) # 0.0
+# print(v.__dict__) # {'_components': array('d', [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]), 'x': 10}
+# print(v) # (0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0)
+
+
+# after adding __setattr__ method.
+vs = Vector(range(10))
+vs._components = array('d', range(5))
+print(vs._components) # array('d', [0.0, 1.0, 2.0, 3.0, 4.0])
+vs.some_field = 42 # OK 
+vs.A = 42 # OK 
+
+# 
+try:
+    vs.x = 777 
+    vs.y = 777 
+    print(vs.x, vs.y)
+except Exception as e:
+    print(e) # readonly attribute 'x'
+    
 
