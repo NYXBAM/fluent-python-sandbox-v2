35761c1cc9b84a442c46a9a4081455e9a29cf745
NYXBAM <delirium.dev@proton.me>
2026-04-22 00:10:39 -0700

    lazy evaluation geneartor: example

---
+++ chapter17/gen_examples.py
@@ -0,0 +1,37 @@
+def gen_AB():
+    print('start')
+    yield 'A'
+    print('continue')
+    yield 'B'
+    print('end.')
+    
+
+res1 = [x*3 for x in gen_AB()]
+# it's eager func
+
+print(res1)
+'''
+start
+continue
+end.
+['AAA', 'BBB']
+'''
+for i in res1:
+    print('-->', i)
+'''
+--> AAA
+--> BBB
+'''
+
+res2 = (x*3 for x in gen_AB())
+# it's lazy evaluation
+print(res2) # <generator object <genexpr> at 0x101c629b0>
+for i in res2:
+    print('-->', i)
+'''
+start
+--> AAA
+continue
+--> BBB
+end.
+'''
