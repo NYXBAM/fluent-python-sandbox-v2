935387b31450b0687ecec8828678984c0713cc16
NYXBAM <delirium.dev@proton.me>
2026-04-24 00:28:04 -0700

    Generator expression: arithmetic progression

---
+++ chapter17/arithmetic_progression.py
@@ -0,0 +1,46 @@
+
+
+class ArithmeticProgression:
+    
+    def __init__(self, begin, step, end=None):
+        self.begin = begin 
+        self.step = step
+        self.end = end 
+        
+    def __iter__(self):
+        result_type = type(self.begin + self.step)
+        result = result_type(self.begin)
+        forever = self.end is None
+        index = 0
+        while forever or result < self.end:
+            yield result
+            index += 1
+            result = self.begin + self.step * index
+            
+
+
+# But easiest way use generator func
+def aritprog_gen(begin, step, end=None):
+    result = type(begin + step)(begin)
+    forever = end is None 
+    index = 0
+    while forever or result < end:
+        yield result
+        index += 1
+        result = begin + step * index
+            
+ap = ArithmeticProgression(0, 1, 5)
+print(list(ap)) # [0, 1, 2, 3, 4]
+
+ap = ArithmeticProgression(1, .5, 3)
+print(list(ap)) # [1.0, 1.5, 2.0, 2.5]
+
+
+ap = ArithmeticProgression(0, 1/3, 3)
+print(list(ap))  # [0.0, 0.3333333333333333, 0.6666666666666666, 1.0, 1.3333333333333333, 1.6666666666666665, 2.0, 2.333333333333333, 2.6666666666666665]
+
+from fractions import Fraction
+
+ap = ArithmeticProgression(0, Fraction(1,3), 1)
+print(list(ap)) # [Fraction(0, 1), Fraction(1, 3), Fraction(2, 3)]
+
