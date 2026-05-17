82eb4f314510d837bcf0216be0af050059b14e41
NYXBAM <delirium.dev@proton.me>
2026-05-17 00:32:24 -0700

    feat: add Procedure class for Lisp interpreter exploration

--- chapter18/lis.py
+++ chapter18/lis.py
@@ -115,6 +115,19 @@ def evaluate(exp: Expression, env: Environment) -> Any:
         case _:
             raise SyntaxError(lispstr(exp))
 
+class Procedure:
+    
+    def __init__(self, parms: list[Symbol], body: list[Expression], env: Environment):
+        self.parms = parms 
+        self.body = body 
+        self.env = env 
+        
+    def __call__(self, *args: Expression) -> Any:
+        local_env = dict(zip(self.parms, args))
+        env = Environment(local_env, self.env)
+        for exp in self.body:
+            result = evaluate(exp, env)
+        return result
 
 
 print(parse('1,5')) # 1,5 
