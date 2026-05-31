59213894efaddd64a0d3c0ff3d2d3d783066ca0f
NYXBAM <delirium.dev@proton.me>
2026-05-31 00:45:17 -0700

    feat: allow switching between Thread/Process pool executors

--- chapter20/flags_threadpool_futures.py
+++ chapter20/flags_threadpool_futures.py
@@ -2,12 +2,15 @@ from concurrent import futures
 
 from flags_threadpool import download_one
 from flags import main
+import time 
 
 
+is_process = True  # Switch to threading or proc
 
 def download_many(cc_list: list[str]) -> int: 
-    cc_list = cc_list[:5]
-    with futures.ThreadPoolExecutor(max_workers=3) as executor:
+    # cc_list = cc_list[:5]
+    executor_cls = futures.ProcessPoolExecutor if is_process else futures.ThreadPoolExecutor
+    with executor_cls(max_workers=8) as executor:
         to_do: list[futures.Future] = []
         for cc in sorted(cc_list):
             future = executor.submit(download_one, cc)
@@ -17,7 +20,7 @@ def download_many(cc_list: list[str]) -> int:
         for count, future in enumerate(futures.as_completed(to_do), 1):
             res: str = future.result()
             print(f'{future} result: {res!r}')
-    
+
     return count 
 
 
