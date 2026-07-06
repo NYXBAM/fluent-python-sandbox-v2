82c84aa1fef942f17d99650da663bee1ad6f58c2
NYXBAM <delirium.dev@proton.me>
2026-07-06 00:20:20 -0700

    Finish chapter 22

--- chapter22/playground.py
+++ chapter22/playground.py
@@ -30,3 +30,14 @@ Class.data = property(lambda self: 'the "data" prop value')
 print(obj.data)  # the "data" prop value
 del Class.data
 print(obj.data)  # bar
+
+
+print(type(obj))
+print(obj.__class__)
+
+print(dir(obj))  # ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__',
+# '__eq__', '__firstlineno__', '__format__', '__ge__', '__getattribute__',
+# '__getstate__', '__gt__', '__hash__', '__init__', '__init_subclass__',
+# '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__',
+# '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__static_attributes__',
+# '__str__', '__subclasshook__', '__weakref__', 'data', 'prop']
